33a1d992ddab5479a062a058b7dfb8fc64c49c75
JeurgenGutsch <juergen@gutsch-online.de>
2021-04-01 21:37:34 +0200

    adds auth to the feedkitchen

---
+++ src/FeedKitchen.Doorman/Config.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace FeedKitchen.Doorman
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
+                },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "scope2" }
+                },
+                // chefportal
+                new Client
+                {
+                    ClientName = "chefportal_client",
+                    ClientId = "chefportal_client",
+                    ClientSecrets = {new Secret("chefportal_client_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = true,
+                    AllowOfflineAccess = true,
+                    AlwaysSendClientClaims = true,
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    RedirectUris = {
+                        "https://localhost:5002/signin-oidc",
+                    },
+                    PostLogoutRedirectUris = {
+                        "https://localhost:5002/signout-callback-oidc",
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess
+                    }
+                },
+                // cookportal
+                new Client
+                {
+                    ClientName = "cookportal_client",
+                    ClientId = "cookportal_client",
+                    ClientSecrets = {new Secret("cookportal_client_secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = true,
+                    AllowOfflineAccess = true,
+                    AlwaysSendClientClaims = true,
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    AlwaysIncludeUserClaimsInIdToken = true,
+                    RedirectUris = {
+                        "https://localhost:5003/signin-oidc",
+                    },
+                    PostLogoutRedirectUris = {
+                        "https://localhost:5003/signout-callback-oidc",
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess
+                    }
+                }
+            };
+    }
+}
