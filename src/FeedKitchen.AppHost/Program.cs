97db1dbae4c8f00e2f790e2f91eb412aff54d0f5
Jürgen Gutsch <juergen.gutsch@yoo.digital>
2025-03-28 00:35:34 +0100

    Refactor solution to replace FeedKitchen.Database with FeedKitchen.Sql
    - Removed the `FeedKitchen.Database` project from the solution.
    - Added a new `FeedKitchen.Sql` project with its own properties and references.
    - Updated the `GlobalSection` in `FeedKitchen.sln` for the new project configurations.
    - Modified project reference order in `Program.cs`.
    - Enhanced `launchSettings.json` with a new HTTPS profile and environment variables.
    - Deleted `FeedKitchen.Database.sqlproj` and introduced a simplified `FeedKitchen.Sql.sqlproj`.

--- src/FeedKitchen.AppHost/Program.cs
+++ src/FeedKitchen.AppHost/Program.cs
@@ -2,13 +2,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var database = builder.AddConnectionString("sql");
 
+
+builder.AddProject<Projects.FeedKitchen_Doorman>("FeedKitchen-Doorman")
+    .WithReference(database);
+
 builder.AddProject<Projects.FeedKitchen_ManagerPortal>("FeedKitchen-ManagerPortal")
     .WithReference(database);
 
 builder.AddProject<Projects.FeedKitchen_ChefPortal>("FeedKitchen-ChefPortal")
-    .WithReference(database);
-
-builder.AddProject<Projects.FeedKitchen_Doorman>("FeedKitchen-Doorman")
     .WithReference(database);
 
 builder.AddProject<Projects.FeedKitchen_Waiter>("FeedKitchen-Waiter")
